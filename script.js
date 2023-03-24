ebd0a591e77c1a28060a16cd7ac4a01d17ba6174
jamshid <jamshidshamshod0705@gmail.com>
2023-03-24 23:26:15 +0500

    callback functions

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ createBook('Buyuklik',4,500)
 createBook('Olma',8)
 createBook()
 console.log(book)
+
+//////////////////////////////////// callback functions //////////////////
+// const arr=['jamshid','xatamov']
+// console.log(arr.join(','))
+
+const addWords=function(str){
+  const newArray=str.split(' ')
+  return newArray
+}
+
+const upperFirstWord=function(str){
+  const [first,...other]=str.split(' ')
+  return [first.toUpperCase(),...other].join()
+}
+
+const transform=function(str,callback){
+  console.log('Orginal str:',str)
+  console.log('Transformed str:',callback(str))
+  console.log('Func name:',callback.name)
+}
+
+transform('jamshid is very interested person !',addWords)
+transform('jamshid is very interested person !',upperFirstWord);
